0c95060e12039b21aa8d7332b410eb142b65b4d7
ajeetht <ajeetht21@gmail.com>
2024-03-20 22:08:11 +0530

    Added education and skill changes

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,16 +1,22 @@
 <script>
   import { Accordion, AccordionItem } from "flowbite-svelte";
-  import {getContext} from "svelte";
-  
+  import { getContext } from "svelte";
+  import { Listgroup, ListgroupItem } from "flowbite-svelte";
+  import PLagugages from "./PLanguages.svelte";
+  import FTechs from "./FTechs.svelte";
+
   const i18n = getContext("i18n");
 </script>
 
 <div
-  class="mainBody text-[#8892b0] bg-[#0a192f] p-[20px] flex flex-col justify-center text-left lg:ml-[10%] lg:mr-[10%] h-[75vh]"
+  class="mainBody text-[#8892b0] bg-[#0a192f] p-[20px] flex flex-col justify-center text-left lg:ml-[10%] lg:mr-[10%]"
 >
   <div class="workExperience">
-    <h2>{$i18n.t("workExp")}</h2>
-      <Accordion activeClass="bg-[#8892b0] text-[#0a192f]" inactiveClass= "hover:text-[#64ffda]">
+      <h2 class="font-bold text-[#bacbff]">{$i18n.t("workExp")}</h2>
+    <Accordion
+      activeClass="bg-[#8892b0] text-[#0a192f]"
+      inactiveClass="hover:text-[#64ffda]"
+    >
       <AccordionItem>
         <span slot="header">{$i18n.t("cseaAccenture")}</span>
         <p>{$i18n.t("cseaAccentureDesc")}</p>
@@ -22,15 +28,35 @@
     </Accordion>
   </div>
   <div class="education">
-    <h2>{$i18n.t("education")}</h2>
-      <Accordion activeClass="bg-[#8892b0] text-[#0a192f]" inactiveClass= "hover:text-[#64ffda]">
+    <h2 class="font-bold text-[#bacbff]">{$i18n.t("education")}</h2>
+    <Listgroup class="bg-[#0a192f] p-4">
+      <ListgroupItem itemDefaultClass="p-2 hover:text-[#64ffda]">{$i18n.t("bTech")}</ListgroupItem>
+      <ListgroupItem itemDefaultClass="p-2 hover:text-[#64ffda]">{$i18n.t("12th")}</ListgroupItem>
+      <ListgroupItem itemDefaultClass="p-2 hover:text-[#64ffda]">{$i18n.t("10th")}</ListgroupItem>   
+    </Listgroup>
+  </div>
+  <div class="skills">
+    <h2 class="font-bold text-[#bacbff]">{$i18n.t("Skills")}</h2>
+    <Accordion>
       <AccordionItem>
-        <span slot="header">{$i18n.t("bTech")}</span>
-        <p>{$i18n.t("cseaAccentureDesc")}</p>
+        <span slot="header">{$i18n.t("planguages")}</span>
+        <PLagugages/>
       </AccordionItem>
       <AccordionItem>
-        <span slot="header">{$i18n.t("aseaAccenture")}</span>
-        <p>{$i18n.t("aseaAccentureDesc")}</p>
+        <span slot="header">{$i18n.t("frontEnd")}</span>
+        <FTechs/>
+      </AccordionItem>
+      <AccordionItem>
+        <span slot="header">{$i18n.t("backEnd")}</span>
+      </AccordionItem>
+      <AccordionItem>
+        <span slot="header">{$i18n.t("databases")}</span>
+      </AccordionItem>
+      <AccordionItem>
+        <span slot="header">{$i18n.t("tools")}</span>
+      </AccordionItem>
+      <AccordionItem>
+        <span slot="header">{$i18n.t("otherTechs")}</span>
       </AccordionItem>
     </Accordion>
   </div>
---
+++ src/routes/about/FTechs.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { Tooltip } from 'flowbite-svelte';
+</script>
+
+
+<div class="flex flex-col">
+    <div class="flex flex-row p-2">
+    <p>Work Experience/Intermediate Experience - </p>
+    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg" class="w-[40px] h-[40px] hover:w-[50px] hover:h-[50px]" alt="ReactJS"/>
+    <Tooltip type="auto">ReactJS</Tooltip>
+    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original-wordmark.svg" class="w-[40px] h-[40px] hover:w-[50px] hover:h-[50px]" alt="tailWindCSS"/>
+    <Tooltip type="auto">TailWind CSS</Tooltip>
+    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/redux/redux-original.svg" class="w-[40px] h-[40px] hover:w-[50px] hover:h-[50px]" alt="Redux"/>
+    <Tooltip type="auto">Redux</Tooltip>
+    </div>
+    <div class="flex flex-row p-2">
+    <p>Learning Experience/Beginner Experience - </p>
+    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg" class="w-[40px] h-[40px] hover:w-[50px] hover:h-[50px]" alt="NextJS"/>
+    <Tooltip type="auto">NextJS</Tooltip>
+    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/svelte/svelte-original.svg" class="w-[40px] h-[40px] hover:w-[50px] hover:h-[50px]" alt="Svelte"/>
+    <Tooltip type="auto">Svelte</Tooltip>
+    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dot-net/dot-net-original.svg" class="w-[40px] h-[40px] hover:w-[50px] hover:h-[50px]" alt="WPF"/>
+    <Tooltip type="auto">Windows Presentation Framework</Tooltip>
+    </div>
+    
+</div>
